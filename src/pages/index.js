1ee218b887746cd54d692cafdb5deebccc4bae99
Ian Sinnott <ian@iansinnott.com>
2020-05-28 13:33:01 +0800

    Minor tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Techstack Podcast" />
-    <h1>🎙</h1>
+    <h1>
+      <span role="img" aria-label="microphone">
+        🎙
+      </span>
+    </h1>
     <h1>Techstack</h1>
     <h2>A podcast about technology</h2>
     <p>🚧 Coming Soon 🚧</p>
