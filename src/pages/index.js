4c04c725eb9bf0861515212b55db42598b5681df
Ian Sinnott <ian@iansinnott.com>
2020-05-28 13:31:21 +0800

    Add techstack podcast site

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,11 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Techstack Podcast" />
+    <h1>🎙</h1>
+    <h1>Techstack</h1>
+    <h2>A podcast about technology</h2>
+    <p>🚧 Coming Soon 🚧</p>
   </Layout>
 )
 
