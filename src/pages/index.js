3816002b23342a0bb1434eb3ed54683e0ef4286a
Ian Sinnott <ian@iansinnott.com>
2020-09-09 21:01:17 +0800

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,15 @@ const IndexPage = () => (
     </h1>
     <h1>Techstack</h1>
     <h2>A podcast about technology</h2>
-    <p>🚧 Coming Soon 🚧</p>
+    <h3>🚧 On hold... indefinitely 🚧</h3>
+    <p>
+      I took a step back from this project because I wasn't feeling inspired. I
+      was <em>just</em> a bit late in creating this podcast, since it would have
+      gone well with my search for more technical depth. I'm still searching,
+      but I'm also interested in things like growing my business and learning
+      Clojure... anyway, feel free to message me on Twitter:{" "}
+      <a href="https://twitter.com/ian_sinn">@ian_sinn</a>
+    </p>
   </Layout>
 )
 
